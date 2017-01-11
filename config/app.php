1cb04b23bb71ee8a3649fa09a2c3ba89d3dd65a0
osette <osettenunez@gmail.com>
2017-01-11 19:40:10 +0800

    Composer update;

--- config/app.php
+++ config/app.php
@@ -178,6 +178,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        /*
+         * Packages
+         */
+        TwigBridge\ServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
     ],
 
     /*
@@ -225,6 +231,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Carbon'   => Carbon\Carbon::class,
+        'Twig' => TwigBridge\Facade\Twig::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
